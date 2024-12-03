7a613f530f113739bee479b1b4dbc13bce60b2e7
kiwired <maksim-doronin93@mail.ru>
2024-12-03 07:58:31 +0200

    feat: funcs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
-// Cписок доменов для обработки
-const domains = `site:sellvia.com`
+const fs = require('fs'); 
+
+async function main() {
+
+	console.log('Старт')
+
+	const rows = await readData();
+	console.log('Найдено слов:', rows.length)
+
+	// Запускаем обработку
+	const res = [];
+
+	for await (const query of rows) {
+		try {
+			const count = await getGoogleResultsCount(query);
+			res[res.length] = `${query},${count}`;
+		} catch (error) {
+			console.log('Не удалось обработать запрос: ', query);
+		}
+	}
+
+	await writeData(res.join("\n"))
+	console.log('Завершено')
+}
+
+main();
+
+function readData(file = './data.csv') {
+	let rows = [];
+	return new Promise((res, rej) => {
+		fs.createReadStream(file)
+			.on('data', (chunk) => {
+				const data = chunk
+					.toString()
+					.split("\n")
+					.map((v) => v.trim())
+					.filter((v) => v)
+
+				rows = rows.concat(data);
+			})
+			.on('end', () => {
+				res(rows);
+			})
+	});
+}
+
+function writeData(data, file = './result.csv') {
+	return new Promise((res, rej) => {
+		fs.writeFile(file, data, (err) => {
+			if (err) {
+				return rej(err);
+			}
+			res();
+		});
+	});
+}
 
 // Функция для отправки запроса к Google и парсинга результатов
 function getGoogleResultsCount(query) {
@@ -8,44 +62,5 @@ function getGoogleResultsCount(query) {
 	url += `&cx=${process.env.GOO_SEARCH_CX}`;
 	return fetch(url)
 		.then((res) => res.json())
-		.then(console.log)
 		.then((res) => res.searchInformation.totalResults)
-		.catch((err) => {
-			reject(new Error(err.message))
-		})
 }
-
-// Функция для обработки списка доменов
-function processDomains(domains) {
-	if (!domains.length) {
-		return
-	}
-
-	const qeury = domains.shift()
-
-	if (!qeury.trim()) {
-		processDomains(domains)
-		return
-	}
-
-	getGoogleResultsCount(qeury)
-		.then((count) => {
-			// console.log(`Для ${qeury} найдено ${count} результатов в Google.`)
-			processDomains(domains)
-		})
-		.catch((error) => {
-			console.log(`Ошибка при сканировании ${qeury}: ${error.message}`)
-			processDomains(domains)
-		})
-}
-
-const parseDomains = domains
-	.replace(" ", ",")
-	.split(",")
-	.map((v) => v.trim())
-	.filter((v) => v)
-	.map((v) => `%22${v}%22`)
-
-console.log("Старт:", parseDomains)
-// Запускаем обработку
-processDomains(parseDomains)
